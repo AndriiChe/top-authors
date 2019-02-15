d9f7d3a49f5fa587095127b8587702db0a735de5
unknown <itppidtrymka@gmail.com>
2019-02-15 09:36:27 +0200

    Updates

---
+++ precache-manifest.48236625f3296bae30776cef6e906d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AndriiChe/top-authors/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62e39cf58550f7b88ea4",
+    "url": "/AndriiChe/top-authors/static/js/runtime~main.62e39cf5.js"
+  },
+  {
+    "revision": "9a5a22e8b3810f5fc2af",
+    "url": "/AndriiChe/top-authors/static/js/main.9a5a22e8.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/AndriiChe/top-authors/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "9a5a22e8b3810f5fc2af",
+    "url": "/AndriiChe/top-authors/static/css/main.3246d4d6.chunk.css"
+  },
+  {
+    "revision": "1b6357df6eec2918954d866a37332a3e",
+    "url": "/AndriiChe/top-authors/index.html"
+  }
+];
